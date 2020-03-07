e6c50a08b6c2271a2a6f6f694d439b8cc0a970c3
Delacroix <unmask429@gmail.com>
2020-03-07 08:17:15 -0800

    mini-web-v2

---
+++ 06-WSGI及mini-web框架/08-运行web服务器时指定端口以及框架/dynamic/mini_frame.py
@@ -0,0 +1,25 @@
+def index():
+    # 如果以web_server.py启动，则文件相对路径都是以web_server.py计算
+    with open("./templates/index.html") as f:
+        content = f.read()
+    return content
+
+
+def center():
+    with open("./templates/center.html") as f:
+        content = f.read()
+    return content
+
+
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
+
+    file_name = env['PATH_INFO']
+    # file_name = '/index.py'
+
+    if file_name == "/index.py":
+        return index()
+    elif file_name == "/center.py":
+        return center()
+    else:
+        return 'Hello World! 我爱你中国···'
